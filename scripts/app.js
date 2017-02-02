f9ac39192a80004d84eb63a93f3de109506a87c1
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2017-02-02 14:31:39 -0500

    render

--- scripts/app.js
+++ scripts/app.js
@@ -24069,6 +24069,7 @@
 	  }, {
 	    key: "render",
 	    value: function render() {
+	      console.log(this.state.items);
 	      return _react2.default.createElement(
 	        "div",
 	        null,
