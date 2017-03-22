1a667f99192fe3983d5bd43a3e09671754334169
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2017-03-22 10:40:35 -0400

    Commit

--- scripts/app.js
+++ scripts/app.js
@@ -24405,6 +24405,8 @@
 	
 	var _react2 = _interopRequireDefault(_react);
 	
+	var _helpers = __webpack_require__(/*! ../../util/helpers */ 206);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -24423,13 +24425,17 @@
 	  }
 	
 	  _createClass(WellWithItem, [{
+	    key: "componentDidMount",
+	    value: function componentDidMount() {
+	      (0, _helpers.procEvent)(window.document, "DOMContentLoaded");
+	    }
+	  }, {
 	    key: "render",
 	    value: function render() {
 	      var _props$item = this.props.item,
 	          body = _props$item.body,
 	          assetUrl = _props$item.assetUrl;
 	
-	      console.log(body);
 	      return _react2.default.createElement(
 	        "div",
 	        { className: "wellwith" },
